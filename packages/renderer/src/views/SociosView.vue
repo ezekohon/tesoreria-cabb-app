f4f59ab81ee1dc99028cb82d47e7ef8d917e7664
Ezequiel Kohon <ezequiel.kohon@zaelot.com>
2023-10-13 11:50:33 -0300

    Primera configuracion e integracion con Firebase

---
+++ packages/renderer/src/views/SociosView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>SOCIOS</div>
+  <pre>{{ documents }}</pre>
+</template>
+
+<script setup lang="ts">
+import {useCollection} from 'vuefire';
+import {sociosRef} from '../firebase';
+
+const documents = useCollection(sociosRef);
+</script>
+
+<style scoped></style>
